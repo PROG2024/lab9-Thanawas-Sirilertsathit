b69822e286df0d9c1fbc7aedc5ed6579cd2daf17
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-03-12 15:39:10 +0700

    a circle test for 3 test cases
    test for constructing negative radius circle
    test for normal add area
    test for add area with zero radius circle

--- test_circle.py
+++ test_circle.py
@@ -8,5 +8,34 @@ Write unit tests as described in README.md.
 
 """
 from circle import Circle
-
+import unittest
 # TODO write 3 tests as described above
+class CircleTest(unittest.TestCase):
+    def test_negative_radius_construction(self):
+        """Illegal case : Trying to construct a circle with negative number radius"""
+        with self.assertRaises(ValueError):
+            c1 = Circle(-2)
+    def test_add_area_normal_case(self):
+        """Legal case : Trying to add area of two circles to get a bigger circle"""
+        c1 = Circle(3)
+        self.assertEqual(3,c1.get_radius())
+        c2 = Circle(4)
+        self.assertEqual(4,c2.get_radius())
+        c3 = c1.add_area(c2)
+        self.assertEqual(5,c3.get_radius())
+        c4 = c2.add_area(c1)
+        self.assertEqual(5,c4.get_radius())
+    def test_add_area_with_radius_zero(self):
+        """Edge case : Trying to add area of two circles which one of them is zero radius"""
+        c1 = Circle(3)
+        self.assertEqual(3,c1.get_radius())
+        c2 = Circle(0)
+        self.assertEqual(0,c2.get_radius())
+        c3 = c1.add_area(c2)
+        self.assertEqual(3,c3.get_radius())
+        c4 = c2.add_area(c1)
+        self.assertEqual(3,c4.get_radius())
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
+    
